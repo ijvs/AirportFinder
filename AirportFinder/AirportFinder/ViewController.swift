487f0fafb7776daff2d8d191ffbed04ffe45070c
Jonathan Velazquez <ijonathanvs@gmail.com>
2020-02-25 12:18:04 -0300

    first commit

---
+++ AirportFinder/AirportFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirportFinder
+//
+//  Created by Israel Jonathan Velázquez Sánchez on 2/25/20.
+//  Copyright © 2020 Siker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
